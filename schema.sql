f48cb2ec8ac917953b25a6ffc3078d8b0e633046
Tony <anthonyhendrickson7@gmail.com>
2021-02-09 16:08:15 -0500

    With retirement_info.csv exported to desktop

--- schema.sql
+++ schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE dept_manager (
     to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
@@ -31,8 +31,7 @@ CREATE TABLE salaries (
   	salary INT NOT NULL,
   	from_date DATE NOT NULL,
   	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 CREATE TABLE department_employee (
@@ -41,15 +40,43 @@ CREATE TABLE department_employee (
   	from_date DATE NOT NULL,
   	to_date DATE NOT NULL,
   	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  	PRIMARY KEY (emp_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title VARCHAR(30) NOT NULL,
+	title VARCHAR NOT NULL,
   	from_date DATE NOT NULL,
   	to_date DATE NOT NULL,
-  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
+
+-- Retirement Eligibility - print to file
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- 1953 Birthdates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+-- 1954 Birthdates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+-- 1955 Birthdates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+SELECT * FROM retirement_info;
